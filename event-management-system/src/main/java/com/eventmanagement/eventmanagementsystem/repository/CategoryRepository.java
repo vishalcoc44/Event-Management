d266947fe2cea7eeaec63659172b263f89876f19
Vishal S <130894175+vishalcoc44@users.noreply.github.com>
2024-12-21 11:12:26 +0530

    Delete event-management-system directory

--- event-management-system/src/main/java/com/eventmanagement/eventmanagementsystem/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventmanagement.eventmanagementsystem.repository;
-
-
-
-import com.eventmanagement.eventmanagementsystem.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- event-management-system/src/main/java/com/eventmanagement/eventmanagementsystem/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventmanagement.eventmanagementsystem.repository;
-
-import com.eventmanagement.eventmanagementsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); // Return an Optional instead of User
-}
